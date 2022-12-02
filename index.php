9f9b57e9ac244468879029cab22a1f6050ffd390
Badvil <andrewkhaiau@gmail.com>
2022-12-02 12:34:04 -0800

    Index.php added

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
-<?php
-        echo "Hello world!"
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initail-scale+1.0">
+    <link rel+"stylesheet" href+style.css">
+    <title>Brushlands</title>
+</html>
 
-        ?>
+<body>
+    <center><h1>Brushlands</h1></center>
+    <div class="container">
+        <div class="table list">
+            <table>
+                <tr>
+                    <th>SNACK AMOUNT</th>
+                </tr>
+                <tr>
+                    <td>__</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    <div class="table form">
+        <table>
+            <form action="insert.php" method="post">
+            <tr>
+                <th>Snacks</th>
+                <td><input type="test" placeholder="Enter SKU (8)"></td>
+            </tr>
+            <tr>
+                <td><button type>Submit</button> </td>
+            </tr>
+            </form>
+        </table>
+    </div>
+    <div class="table count"></div>
+</div>
+</body>
